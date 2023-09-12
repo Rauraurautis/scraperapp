366b25a37b0a1740c812a810a26080a87dbf7e58
Rauraurautis <rautanokka@gmail.com>
2023-09-12 19:54:04 +0300

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import { scrapeToriAxios } from './lib/utils/scraper';
 import Item from './components/Item';
 import notification from "./notification.mp3"
 import useSound from 'use-sound';
+import { isMobile } from 'react-device-detect';
 
 export interface ItemInfo {
   item: string
   link: string
-  image: string
+  image: string | null
 }
 
 const audio = new Audio(notification)
 
 function App() {
   const [items, setItems] = useState<ItemInfo[]>([])
-  const [image, setImage] = useState<string | null>(null)
+  const [highlightedItem, setHighlightedItem] = useState<ItemInfo | null>(null)
   const [sound, setSound] = useState(false)
 
   useEffect(() => {
@@ -41,14 +42,21 @@ function App() {
   }, [items])
 
 
-
-
   return (
-    <div className="bg-slate-900 w-screen h-screen flex flex-col justify-center items-center">
-      <div className="w-[150px] h-[150px] md:w-[250px] md:h-[250px]">{image ? <img className="relative top-0 invisible md:visible" src={image ?? ""} /> : ""}</div>
-      <ul className=" bg-slate-800 p-5 max-h-[50%] w-fit overflow-y-scroll overflow-x-hidden flex flex-col items-center">
+    <div className="bg-slate-900 w-screen h-screen flex flex-col gap-5 justify-center items-center">
+      <div className="w-[150px] h-[150px] md:w-[250px] md:h-[250px] flex justify-center">
+        {isMobile && highlightedItem?.image ?
+          <div className="flex flex-col justify-center items-center">
+            <img className="relative" src={highlightedItem.image ?? ""} />
+            <a className="text-white py-3 text-xl" href={highlightedItem.link}>Go to product</a>
+          </div> : highlightedItem?.image ?
+            <img className="relative top-0" src={highlightedItem.image ?? ""} /> : <h1 className="text-white text-center text-3xl md:text-7xl absolute top-2">Tori annetaan-scraper beta</h1>}
+
+      </div>
+
+      <ul className=" bg-slate-800 p-5 max-h-[50%] w-fit overflow-y-scroll overflow-x-hidden flex flex-col items-center rounded-xl">
         {items.length > 0 ? items.map((item, i) => (
-          <Item item={item} index={i} setImage={setImage} key={i} />
+          <Item item={item} index={i} setHighlightedItem={setHighlightedItem} key={i} />
         )
         ) : <h1 className="text-white font-semibold">Loading tori items..</h1>
         }
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,20 +1,33 @@
 import { FC, useState } from 'react'
 import { ItemInfo } from '../App'
-
+import { isMobile } from 'react-device-detect'
 
 interface ItemProps {
     item: ItemInfo
     index: number
-    setImage: React.Dispatch<React.SetStateAction<string | null>>
+    setHighlightedItem:  React.Dispatch<React.SetStateAction<ItemInfo | null>>
 }
 
-const Item: FC<ItemProps> = ({ item, index, setImage }) => {
+const Item: FC<ItemProps> = ({ item, index, setHighlightedItem }) => {
     const [showImage, setShowImage] = useState(false)
 
+    if (isMobile) {
+        return <li key={index} className="text-white font-semibold hover:scale-125 transition-all"
+            onMouseEnter={() => setHighlightedItem(prev => item)}
+            onMouseLeave={() => setHighlightedItem(prev => null)}
+            onClick={() => setHighlightedItem(prev => item)}>
+
+            <p>{item.item}</p>
+
+
+
+        </li>
+    }
+
     return <li key={index} className="text-white font-semibold hover:scale-125 transition-all"
-        onMouseEnter={() => setImage(prev => item.image)}
-        onMouseLeave={() => setImage(prev => null)}>
-        <a href={item.link}>{item.item}</a>
+        onMouseEnter={() => setHighlightedItem(prev => item)}
+        onMouseLeave={() => setHighlightedItem(prev => null)}>
+        <a href={item.link + "#"}>{item.item}</a>
 
 
 
